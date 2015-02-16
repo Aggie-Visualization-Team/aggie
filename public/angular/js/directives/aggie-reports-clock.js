13df8ab7e7833e425502cde6a0853a0aa9fca9e0
Trey Washington <lt.washington9@gmail.com>
2015-02-16 18:58:48 -0500

    removed hour hand and added minutes

--- public/angular/js/directives/aggie-reports-clock.js
+++ public/angular/js/directives/aggie-reports-clock.js
@@ -7,40 +7,26 @@ angular.module('Aggie')
                 link: function($scope, element, attributes) {
                     var radians = 0.0174532925,
                         clockRadius = 200,
-                        margin = 100,
+                        margin = 120,
                         width = (clockRadius+margin)*2,
                         height = (clockRadius+margin)*2,
-                        hourHandLength = 2*clockRadius/3,
                         minuteHandLength = clockRadius,
                         secondHandLength = clockRadius-12,
                         secondHandBalance = 30,
                         secondTickStart = clockRadius,
                     secondTickLength = -10,
-                        hourTickStart = clockRadius,
-                        hourTickLength = -18,
                     secondLabelRadius = clockRadius + 16,
-                    secondLabelYOffset = 5,
-                    hourLabelRadius = clockRadius - 40,
-                    hourLabelYOffset = 7;
+                    secondLabelYOffset = 5;
 
                     var graphDataStart = clockRadius + 25,
-                        graphDatalength = 30;
-
-                    var hourScale = d3.scale.linear()
-                        .range([0,330])
-                        .domain([0,11]);
+                        graphDatalength = 30,
+                        minGraphStart = clockRadius - 100;
 
                     var minuteScale = secondScale = d3.scale.linear()
                         .range([0,354])
                         .domain([0,59]);
 
                     var handData = [
-                        {
-                            type:'hour',
-                            value:0,
-                            length:-hourHandLength,
-                            scale:hourScale
-                        },
                         {
                             type:'minute',
                             value:0,
@@ -97,35 +83,6 @@ angular.module('Aggie')
                                 return d;
                             });
 
-                        //... and hours
-                        face.selectAll('.hour-tick')
-                            .data(d3.range(0,12)).enter()
-                            .append('line')
-                            .attr('class', 'hour-tick')
-                            .attr('x1',0)
-                            .attr('x2',0)
-                            .attr('y1',hourTickStart)
-                            .attr('y2',hourTickStart + hourTickLength)
-                            .attr('transform',function(d){
-                                return 'rotate(' + hourScale(d) + ')';
-                            });
-
-                        face.selectAll('.hour-label')
-                            .data(d3.range(3,13,3))
-                            .enter()
-                            .append('text')
-                            .attr('class', 'hour-label')
-                            .attr('text-anchor','middle')
-                            .attr('x',function(d){
-                                return hourLabelRadius*Math.sin(hourScale(d)*radians);
-                            })
-                            .attr('y',function(d){
-                                return -hourLabelRadius*Math.cos(hourScale(d)*radians) + hourLabelYOffset;
-                            })
-                            .text(function(d){
-                                return d;
-                            });
-
                         renderReportsGraph();
 
                         var hands = face.append('g').attr('id','clock-hands');
@@ -167,9 +124,8 @@ angular.module('Aggie')
 
                     function updateData(){
                         var t = new Date();
-                        handData[0].value = (t.getHours() % 12) + t.getMinutes()/60 ;
-                        handData[1].value = t.getMinutes();
-                        handData[2].value = t.getSeconds();
+                        handData[0].value = t.getMinutes();
+                        handData[1].value = t.getSeconds();
                     }
 
                     drawClock();
@@ -183,30 +139,32 @@ angular.module('Aggie')
                     d3.select(self.frameElement).style("height", height + "px");
 
                     function renderReportsGraph() {
-                        if(handData[2].value == 0 ){
-                            renderHourReportsGraph();
+                        if(handData[1].value == 0 ){
+                            //clear seconds data
+                            d3.select('#clock-face').selectAll('.reports-data.sec').remove();
+                            renderMinReportsGraph();
                         }
-                        var reportsData = formatData(getReportPerSecond(handData[2].value));
+                        var secReportsData = formatData(getReportPerSecond(handData[1].value));
 
                         var graphScale = d3.scale.linear()
                             .range([0, graphDatalength])
-                            .domain([0,( d3.max(reportsData[0].data, function(d){
+                            .domain([0,( d3.max(secReportsData[0].data, function(d){
                                 return d.value;
                             }))]);
 
-                        var report = d3.select('#clock-face').selectAll('reports-data')
-                            .data(reportsData).enter()
+                        var secReport = d3.select('#clock-face').selectAll('reports-data sec')
+                            .data(secReportsData).enter()
                             .append('g')
-                            .attr('class', 'reports-data')
+                            .attr('class', 'reports-data sec')
                             .attr('x1', 0)
                             .attr('x2', 0)
                             .attr('y1', graphDataStart)
                             .attr('y2', graphDataStart + graphDatalength)
                             .attr('transform',function(report){
-                                return 'rotate(' + (180 + (6 * (report.minute - 1))) + ')';
+                                return 'rotate(' + (180 + (6 * (report.second - 1))) + ')';
                             });
 
-                        report.selectAll('rect')
+                        secReport.selectAll('rect')
                             .data(function(d){ return d.data}).enter()
                             .append('rect')
                             .attr('width', 10)
@@ -231,16 +189,91 @@ angular.module('Aggie')
                             .attr("height", function(d) {
                                 return (graphScale(d.y1) - graphScale(d.y0));
                             });
+
+
                     }
 
-                    function renderHourReportsGraph() {
+                    function renderMinReportsGraph() {
+                        var minReportsData = formatData(getReportPerMinute(handData[0].value));
 
+                        var graphScale = d3.scale.linear()
+                            .range([graphDatalength, 0])
+                            .domain([0,( d3.max(minReportsData[0].data, function(d){
+                                return d.value;
+                            }))]);
+
+                        var minReport = d3.select('#clock-face').selectAll('reports-data min')
+                            .data(minReportsData).enter()
+                            .append('g')
+                            .attr('class', 'reports-data min')
+                            .attr('x1', 0)
+                            .attr('x2', 0)
+                            .attr('y1', minGraphStart)
+                            .attr('y2', minGraphStart - graphDatalength)
+                            .attr('transform',function(report){
+                                return 'rotate(' + (180 + (6 * (report.minute - 1))) + ')';
+                            });
+
+                        minReport.selectAll('rect')
+                            .data(function(d){ return d.data}).enter()
+                            .append('rect')
+                            .attr('width', 10)
+                            .attr('x', -5)
+                            .attr('class', function(d) {
+                                switch (d.type) {
+                                    case 'facebook':
+                                        return 'facebook-source';
+                                    case 'twitter':
+                                        return 'twitter-source';
+                                    case 'elmo':
+                                        return 'elmo-source';
+                                    case 'rss':
+                                        return 'rss-source';
+                                    default:
+                                        return 'unknown-source';
+                                }
+                            })
+                            .attr('y', function(d) {
+                                return minGraphStart - graphScale(d.y0);
+                            })
+                            .attr("height", function(d) {
+                                return -(graphScale(d.y1) - graphScale(d.y0));
+                            });
                     }
 
                     function getReportPerSecond(second){
 
                         return [{
-                            minute: second,
+                            second: second,
+                            minute: 0,
+                            hour: 0,
+                            data: [
+                                {
+                                    type: 'twitter',
+                                    value: Math.floor(Math.random() * 1000) + 1
+                                },
+                                {
+                                    type: 'rss',
+                                    value: Math.floor(Math.random() * 1000) + 1
+                                },
+                                {
+                                    type: 'elmo',
+                                    value: Math.floor(Math.random() * 1000) + 1
+                                },
+                                {
+                                    type: 'facebook',
+                                    value: Math.floor(Math.random() * 1000) + 1
+                                }
+                            ]
+                        }]
+
+                    }
+
+                    function getReportPerMinute(minute){
+
+                        return [{
+                            second: 0,
+                            minute: minute,
                             hour: 0,
                             data: [
                                 {
