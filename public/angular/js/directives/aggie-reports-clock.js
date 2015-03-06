5c107cbeaf0305fe4d426bdc7ecc11395b2fa431
Trey Washington <lt.washington9@gmail.com>
2015-03-06 17:40:01 -0500

    Hooked in real data with visualization, No scaleing yet

--- public/angular/js/directives/aggie-reports-clock.js
+++ public/angular/js/directives/aggie-reports-clock.js
@@ -1,9 +1,8 @@
 angular.module('Aggie')
     .directive('clockVisualization', [
-        'Report',
-        'Source',
-        'Batch',
-        function(Report, Source, Batch) {
+        '$resource',
+        '$http',
+        function($resource, $http) {
             return {
                 restrict:'E',
                 link: function($scope, element, attributes) {
@@ -21,23 +20,51 @@ angular.module('Aggie')
                     secondLabelYOffset = 5;
 
                     var graphDataStart = clockRadius + 25,
-                        graphDatalength = 30,
+                        graphDataLength = 30,
                         minGraphStart = clockRadius - 100;
 
                     var minuteScale = secondScale = d3.scale.linear()
                         .range([0,354])
                         .domain([0,59]);
 
+                    var currDate = moment('2014-06-21 04:05:10');
+                    var nextDate = moment('2014-06-21 04:05:10');
+                    nextDate.add(1,'minutes');
+                    nextDate.add(1, 'seconds');
+                    var pastMinData = [];
+                    var rawData = [];
+                    $http({
+                        method: 'GET',
+                        url: '/api/v1/report/viz',
+                        params : {
+                            startTime: currDate,
+                            endTime: nextDate
+                        }
+                    }).success(function(data) {
+                        rawData = data;
+                        pastMinData = formatData(data, currDate.minute(), currDate.second());
+
+                        drawClock();
+
+                        setInterval(function(){
+                            updateData();
+                            moveHands();
+                            displayReportsGraph();
+                        }, 1000);
+
+                        d3.select(self.frameElement).style("height", height + "px");
+                    });
+
                     var handData = [
                         {
                             type:'minute',
-                            value:0,
+                            value:currDate.minute(),
                             length:-minuteHandLength,
                             scale:minuteScale
                         },
                         {
                             type:'second',
-                            value:0,
+                            value:currDate.second(),
                             length:-secondHandLength,
                             scale:secondScale,
                             balance:secondHandBalance
@@ -85,7 +112,7 @@ angular.module('Aggie')
                                 return d;
                             });
 
-                        renderReportsGraph();
+                        displayReportsGraph();
 
                         var hands = face.append('g').attr('id','clock-hands');
 
@@ -125,88 +152,102 @@ angular.module('Aggie')
                     }
 
                     function updateData(){
-                        var t = new Date();
-                        handData[0].value = t.getMinutes();
-                        handData[1].value = t.getSeconds();
+                        currDate.add(1, 'seconds');
+                        nextDate.add(1, 'seconds');
+                        handData[0].value = currDate.minute();
+                        handData[1].value = currDate.second();
                     }
 
-                    drawClock();
-
-                    setInterval(function(){
-                        updateData();
-                        moveHands();
-                        renderReportsGraph();
-                    }, 1000);
-
-                    d3.select(self.frameElement).style("height", height + "px");
-
-                    function renderReportsGraph() {
+                    function displayReportsGraph() {
                         if(handData[1].value == 0 ){
                             //clear seconds data
                             d3.select('#clock-face').selectAll('.reports-data.sec').remove();
-                            renderMinReportsGraph();
+                            displayMinReportsGraph();
                         }
-                        else {
-                            var secReportsData = formatData(getReportPerSecond(handData[1].value));
-
-                            var graphScale = d3.scale.linear()
-                                .range([0, graphDatalength])
-                                .domain([0,( d3.max(secReportsData[0].data, function(d){
-                                    return d.value;
-                                }))]);
-
-                            var secReport = d3.select('#clock-face').selectAll('reports-data sec')
-                                .data(secReportsData).enter()
-                                .append('g')
-                                .attr('class', 'reports-data sec')
-                                .attr('x1', 0)
-                                .attr('x2', 0)
-                                .attr('y1', graphDataStart)
-                                .attr('y2', graphDataStart + graphDatalength)
-                                .attr('transform',function(report){
-                                    return 'rotate(' + (180 + (6 * (report.second - 1))) + ')';
-                                });
-
-                            secReport.selectAll('rect')
-                                .data(function(d){ return d.data}).enter()
-                                .append('rect')
-                                .attr('width', 10)
-                                .attr('x', -5)
-                                .attr('class', function(d) {
-                                    switch (d.type) {
-                                        case 'facebook':
-                                            return 'facebook-source';
-                                        case 'twitter':
-                                            return 'twitter-source';
-                                        case 'elmo':
-                                            return 'elmo-source';
-                                        case 'rss':
-                                            return 'rss-source';
-                                        default:
-                                            return 'unknown-source';
-                                    }
-                                })
-                                .attr('y', function(d) {
-                                    return graphDataStart + graphScale(d.y0);
-                                })
-                                .attr("height", function(d) {
-                                    return (graphScale(d.y1) - graphScale(d.y0));
-                                });
-                        }
+                        getReportPerSecond(renderSecondsReports);
                     }
 
-                    function renderMinReportsGraph() {
-                        if(handData[0].value == 0){
-                            d3.select('#clock-face').selectAll('.reports-data.min').remove();
-                        }
-                        var minReportsData = formatData(getReportPerMinute(handData[0].value));
+                    function getReportPerSecond(render) {
+                        var authoredAt, compVal;
+                        var secondData = rawData.filter(function(report) {
+                            authoredAt = moment(report.authoredAt);
+                            compVal = authoredAt.second() - currDate.second();
+                            return compVal >= 0 && compVal < 2;
+                        });
+                        render(formatData(secondData, currDate.minute(), currDate.second()));
+                    }
+
+                    function renderSecondsReports(secReportsData){
+                        var graphScale = d3.scale.linear()
+                            .range([0, graphDataLength])
+                            .domain([0,( d3.max(pastMinData[0].data, function(d){
+                                return d.value;
+                            }))]);
+
+                        var secReport = d3.select('#clock-face').selectAll('reports-data sec')
+                            .data(secReportsData).enter()
+                            .append('g')
+                            .attr('class', 'reports-data sec')
+                            .attr('x1', 0)
+                            .attr('x2', 0)
+                            .attr('y1', graphDataStart)
+                            .attr('y2', graphDataStart + graphDataLength)
+                            .attr('transform',function(report){
+                                return 'rotate(' + (180 + (6 * (report.second - 1))) + ')';
+                            });
+
+                        secReport.selectAll('rect')
+                            .data(function(d){ return d.data}).enter()
+                            .append('rect')
+                            .attr('width', 10)
+                            .attr('x', -5)
+                            .attr('class', function(d) {
+                                switch (d.type) {
+                                    case 'facebook':
+                                        return 'facebook-source';
+                                    case 'twitter':
+                                        return 'twitter-source';
+                                    case 'elmo':
+                                        return 'elmo-source';
+                                    case 'rss':
+                                        return 'rss-source';
+                                    default:
+                                        return 'unknown-source';
+                                }
+                            })
+                            .attr('y', function(d) {
+                                return graphDataStart + graphScale(d.y0);
+                            })
+                            .attr("height", function(d) {
+                                return (graphScale(d.y1) - graphScale(d.y0));
+                            });
+                    }
+                    function displayMinReportsGraph() {
+                        getReportPerMinute(renderMinReports);
+                    }
+
+                    function getReportPerMinute(render){
+                        $http({
+                            method: 'GET',
+                            url: '/api/v1/report/viz',
+                            params : {
+                                startTime: currDate.toDate(),
+                                endTime: nextDate.toDate()
+                            }
+                        }).success(function(data) {
+                            rawData = data;
+                            pastMinData = formatData(data, currDate.minute(), currDate.second());
+                            render(pastMinData);
+                        });
+                    }
+
+                    function renderMinReports(minReportsData){
 
                         var graphScale = d3.scale.linear()
-                            .range([graphDatalength, 0])
-                            //.domain([0,( d3.max(minReportsData[0].data, function(d){
-                            //    return d.value;
-                            //}))]);
-                            .domain(1000);
+                            .range([graphDataLength, 0])
+                            .domain([0,( d3.max(minReportsData[0].data, function(d){
+                                return d.value;
+                            }))]);
 
                         var minReport = d3.select('#clock-face').selectAll('reports-data min')
                             .data(minReportsData).enter()
@@ -215,7 +256,7 @@ angular.module('Aggie')
                             .attr('x1', 0)
                             .attr('x2', 0)
                             .attr('y1', minGraphStart)
-                            .attr('y2', minGraphStart - graphDatalength)
+                            .attr('y2', minGraphStart - graphDataLength)
                             .attr('transform',function(report){
                                 return 'rotate(' + (180 + (6 * (report.minute - 1))) + ')';
                             });
@@ -245,69 +286,65 @@ angular.module('Aggie')
                             .attr("height", function(d) {
                                 return -(graphScale(d.y1) - graphScale(d.y0));
                             });
+                        if(handData[0].value == 0){
+                            d3.select('#clock-face').selectAll('.reports-data.min').remove();
+                        }
                     }
-                    Report.query().$promise.then(function(result) {
-                          console.log(result.$promise.then(function(data){console.log(data)}) )
-                        });
-                    Batch.load().$promise.then(function(results){console.log(results)});
-                    function getReportPerSecond(second){
 
-                        return [{
+                    function formatData(data, minute, second) {
+                        var y0 = 0;
+                        var twitterVal = 0,
+                            faceVal = 0,
+                            elmoVal = 0,
+                            rssVal = 0,
+                            unknownVal = 0;
+
+                        for (var i = 0; i < data.length; i++) {
+                            switch (data[i]._sourceType) {
+                                case 'twitter':
+                                    twitterVal++;
+                                    break;
+                                case 'facebook':
+                                    faceVal++;
+                                    break;
+                                case 'elmo':
+                                    elmoVal++;
+                                    break;
+                                case 'rss':
+                                    rssVal++;
+                                    break;
+                                default :
+                                    unknownVal++;
+                            }
+                        }
+                        var dataToReturn = [{
                             second: second,
-                            minute: 0,
-                            hour: 0,
-                            data: [
-                                {
-                                    type: 'twitter',
-                                    value: Math.floor(Math.random() * 1000) + 1
-                                },
-                                {
-                                    type: 'rss',
-                                    value: Math.floor(Math.random() * 1000) + 1
-                                },
-                                {
-                                    type: 'elmo',
-                                    value: Math.floor(Math.random() * 1000) + 1
-                                },
-                                {
-                                    type: 'facebook',
-                                    value: Math.floor(Math.random() * 1000) + 1
-                                }
-                            ]
-                        }]
-
-                    }
-
-                    function getReportPerMinute(minute){
-
-                        return [{
-                            second: 0,
                             minute: minute,
                             hour: 0,
                             data: [
                                 {
                                     type: 'twitter',
-                                    value: Math.floor(Math.random() * 1000000) + 1
+                                    value: twitterVal
                                 },
                                 {
                                     type: 'rss',
-                                    value: Math.floor(Math.random() * 1000000) + 1
+                                    value: rssVal
                                 },
                                 {
                                     type: 'elmo',
-                                    value: Math.floor(Math.random() * 1000000) + 1
+                                    value: elmoVal
                                 },
                                 {
                                     type: 'facebook',
-                                    value: Math.floor(Math.random() * 1000000) + 1
+                                    value: faceVal
+                                },
+                                {
+                                    type: 'unknown',
+                                    value: unknownVal
                                 }
                             ]
-                        }]
-
-                    }
-                    function formatData(data){
-                        var y0 = 0;
-                        data[0].data =  data[0].data.map(function(report) {
+                        }];
+                        dataToReturn[0].data =  dataToReturn[0].data.map(function(report) {
                             return {
                                 type: report.type,
                                 value: report.value,
@@ -315,8 +352,7 @@ angular.module('Aggie')
                                 y1: y0 += report.value
                             }
                         });
-
-                        return data;
+                        return dataToReturn
                     }
                 }
             }
